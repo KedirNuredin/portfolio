2e0006f5c8f1ac5dbcdf2a5cdf8be5f92f18b88a
kedirnuredin <webportfolio.kedir@gmail.com>
2024-01-10 15:28:18 +0900

    my first commit

---
+++ components/Nav.vue
@@ -0,0 +1,20 @@
+<template>
+    <div style="width: 1200px; margin: 0 auto;">
+        <b-nav align="end" text-color="black">
+            <b-navbar-brand>Portfolio</b-navbar-brand>
+            <b-nav-item active>About</b-nav-item>
+            <b-nav-item>Link</b-nav-item>
+            <b-nav-item>Projects</b-nav-item>
+            <b-nav-item>Contact Me</b-nav-item>
+        </b-nav>
+    </div>
+</template>
+<script>
+export default {
+    name:'Nav',
+    
+}
+</script>
+<style scoped>
+    
+</style>
